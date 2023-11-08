96b95261c4ad7e15bc9f561152ece7ed922b199c
cjhdev <cam@cjh.id.au>
2023-11-08 04:18:25 +0000

    add module

--- lib/chirp_stack_api.rb
+++ lib/chirp_stack_api.rb
@@ -1,5 +1,9 @@
-$LOAD_PATH.unshift File.expand_path("#{File.dirname(__FILE__)}/generated")
+module ChirpStackAPI
 
-Dir["#{File.dirname(__FILE__)}/generated/**/*_pb.rb"].each { |f| require f }
+    $LOAD_PATH.unshift File.expand_path("#{File.dirname(__FILE__)}/generated")
 
-$LOAD_PATH.shift
+    Dir["#{File.dirname(__FILE__)}/generated/**/*_pb.rb"].each { |f| require f }
+
+    $LOAD_PATH.shift
+
+end
